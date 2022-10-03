4d521d45f3efc79d535e23b3fdb283221bc59905
HailGanyu <henryyanguk@gmail.com>
2022-10-03 15:48:32 +0100

    boilerplate for main.js

--- main.js
+++ main.js
@@ -1,23 +1,106 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
+import * as THREE from "./node_modules/three";
+import { OrbitControls } from "./node_modules/three/addons/controls/OrbitControls.js";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+let renderer, scene, camera;
+let group;
 
-setupCounter(document.querySelector('#counter'))
+let selectedObject = null;
+const raycaster = new THREE.Raycaster();
+const pointer = new THREE.Vector2();
+
+init();
+animate();
+
+function init() {
+
+    // init renderer
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+    // init scene
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color(0xffffff);
+
+    group = new THREE.Group();
+    scene.add(group);
+
+    // init camera
+    camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 1000);
+    camera.position.set(15, 15, 15);
+    camera.lookAt(scene.position);
+
+    const controls = new OrbitControls(camera, renderer.domElement);
+    controls.minDistance = 15;
+    controls.maxDistance = 250;
+
+    // sprites
+    const sprite1 = new THREE.Sprite(new THREE.SpriteMaterial({ color: '#69f' }));
+    sprite1.position.set(6, 5, 5);
+    sprite1.scale.set(2, 5, 1);
+    group.add(sprite1);
+
+    const sprite2 = new THREE.Sprite(new THREE.SpriteMaterial({ color: '#69f', sizeAttenuation: false }));
+    sprite2.material.rotation = Math.PI / 3 * 4;
+    sprite2.position.set(8, - 2, 2);
+    sprite2.center.set(0.5, 0);
+    sprite2.scale.set(.1, .5, .1);
+    group.add(sprite2);
+
+    const group2 = new THREE.Object3D();
+    group2.scale.set(1, 2, 1);
+    group2.position.set(- 5, 0, 0);
+    group2.rotation.set(Math.PI / 2, 0, 0);
+    group.add(group2);
+
+    const sprite3 = new THREE.Sprite(new THREE.SpriteMaterial({ color: '#69f' }));
+    sprite3.position.set(0, 2, 5);
+    sprite3.scale.set(10, 2, 3);
+    sprite3.center.set(- 0.1, 0);
+    sprite3.material.rotation = Math.PI / 3;
+    group2.add(sprite3);
+
+    window.addEventListener('resize', onWindowResize);
+    document.addEventListener('pointermove', onPointerMove);
+}
+
+function animate() {
+
+    renderer.render(scene, camera);
+    requestAnimationFrame(animate);
+}
+
+function onWindowResize() {
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+function onPointerMove(event) {
+
+    if (selectedObject) {
+        selectedObject.material.color.set('#69f');
+        selectedObject = null;
+    }
+
+    pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
+    pointer.y = - (event.clientY / window.innerHeight) * 2 + 1;
+
+    raycaster.setFromCamera(pointer, camera);
+
+    const intersects = raycaster.intersectObject(group, true);
+
+    if (intersects.length > 0) {
+        const res = intersects.filter(function (res) {
+            return res && res.object;
+        })[0];
+
+        if (res && res.object) {
+            selectedObject = res.object;
+            selectedObject.material.color.set('#f00');
+        }
+    }
+}
